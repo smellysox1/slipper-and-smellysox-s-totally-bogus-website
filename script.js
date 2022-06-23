74b7a1804ab99856b5b2abd06b6549c2a5b01128
smellysox1 <80152017+smellysox1@users.noreply.github.com>
2022-06-23 13:53:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+=document.getElementById('good').onclick = function () {
+    location.href = 'good.html';
+}
+
+document.getElementById('bad').onclick = function () {
+    location.href = 'bad.html';
+}
+
+document.getElementById('unsure').onclick = function () {
+    location.href = 'unsure.html';
+}
+
+document.getElementById('stoopid').onclick = function () {
+    location.href = 'stoopid.html';
+}
+
+function good() {
+    location.href = 'good.html'
+}
+
+function bad() {
+    location.href = 'bad.html'
+}
+
+function unsure() {
+    location.href = 'unsure.html'
+}
+
+function stoopid() {
+    location.href = 'stoopid.html'
+}
+
+function taxSubmit(){
+  
+}
+function gohome(){
+  location.href = "index.html;
+}
+function nowayhome(){
+  document.getElementById("noesc").innerHTML = "there is no escape from KERMIT";
+}
